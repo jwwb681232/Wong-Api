b6cf153d695381cf672b5038363843180e9b080d
wangxiao <wx497657341@qq.com>
2018-03-21 11:33:27 +0800

    JwtGuard to EloquentMerchantUserProvider

--- app/Api/V1/Controllers/MerchantUser/UserController.php
+++ app/Api/V1/Controllers/MerchantUser/UserController.php
@@ -8,8 +8,16 @@ class UserController extends Controller
 {
     public function register()
     {
-        return response(
-            auth()->guard('merchant')->attempt(['user_name' => 'wangxiao', 'user_mobile' => 18871478079])
-        );
+        //请求时附带Authorization:bearer eyJ0eXAiOiJKV... 可以获取到用户信息
+        //return response(auth()->guard('merchant')->user());
+
+        //请求时附带Authorization:bearer eyJ0eXAiOiJKV... 可以获取到token payload
+        //return response(auth()->guard('merchant')->payload());
+
+        //根据给定条件可以获取生成的用户token App\Auth\EloquentMerchantUserProvider retrieveByCredentials 配合使用
+        return response(auth()->guard('merchant')->attempt(['user_name' => 'wangxiao', 'user_mobile' => 18871478079]));
+
+        //根据给定主键可以获取生成的用户token App\Models\MerchantUser $primaryKey 配合使用
+        //return response(auth()->guard('merchant')->tokenById(1));
     }
 }
